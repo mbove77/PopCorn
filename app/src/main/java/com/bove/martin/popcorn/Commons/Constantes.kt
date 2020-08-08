a24b8aedd861bcfe0d492a58dc67e1844b3e9aea
mbove77 <mbove77@gmail.com>
2020-08-08 14:56:50 -0300

    Minor Changes

--- app/src/main/java/com/bove/martin/popcorn/Commons/Constantes.kt
+++ app/src/main/java/com/bove/martin/popcorn/Commons/Constantes.kt
@@ -6,8 +6,8 @@ package com.bove.martin.popcorn.Commons
  */
 class Constantes {
     companion object{
-        public val API_URL = "https://api.themoviedb.org/3/"
-        public val API_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
-        public val API_KEY = "b3597ba8495b6f69a741d1b5cd795dd4"
+        val API_URL = "https://api.themoviedb.org/3/"
+        val API_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+        val API_KEY = "b3597ba8495b6f69a741d1b5cd795dd4"
     }
 }
